7a1cb9fe9dc6089cef3cd24cdf72ebf92031f65c
Thariq Luqmana <thariqluckyman@gmail.com>
2022-06-15 08:50:52 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Login from "./pages/auth/Login";
+import Login from "./pages/auth/Login/Login";
 import Registration from "./pages/auth/Registration";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Registration />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
