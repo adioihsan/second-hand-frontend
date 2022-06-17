3f0080483d9a982ab3458c82815e4920d3b979b3
adioihsan <adioihsan@gmail.com>
2022-06-17 18:26:57 +0000

    "Add info profile page"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Guest from "./layouts/Guest";
 import Home from "./pages/home/Home";
 import Register from "./pages/auth/Register/Registration";
+import ProfileInfo from "./pages/profileInfo/ProfileInfo";
+import Buyer from "./layouts/Buyer";
+import ProductAdd from "./pages/productAdd/productAdd";
+import Seller from "./layouts/Seller";
 
 function App() {
   return (
@@ -13,6 +17,12 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route element={<Guest />}>
             <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<Buyer />}>
+            <Route path="/profile-info" element={<ProfileInfo />} />
+          </Route>
+          <Route element={<Seller />}>
+            <Route path="/product-add" element={<ProductAdd />} />
           </Route>
         </Routes>
       </BrowserRouter>
