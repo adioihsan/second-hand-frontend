9d0d7c595392fd6a0d7869a6866e8397f9f6efd9
adioihsan <adioihsan@gmail.com>
2022-06-29 09:03:10 +0000

    add read product for seller

--- src/layouts/Public.jsx
+++ src/layouts/Public.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/navigation/navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import LoadingFull from "../components/loading/lodingFull/LoadingFull";
 import { getLocalJWT, parseJwt } from "../services/utils/jwtHandler";
 import {
   getUserDetail,
@@ -12,9 +13,11 @@ import {
 } from "../services/actions/userAction";
 import { useNavigate } from "react-router-dom";
 function Public(props) {
+  const { token, userDetail, userData } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { token, userData } = useSelector((state) => state.user);
+  //
+  const [isGuest, setIsGuest] = useState(false);
 
   useEffect(() => {
     try {
@@ -22,24 +25,27 @@ function Public(props) {
         const localToken = getLocalJWT();
         dispatch(setUserToken(localToken));
         const user = parseJwt(localToken);
+        console.log(user);
         dispatch(setUserData(user));
         dispatch(getUserDetail());
       }
     } catch (error) {
-      // console.log(error);
+      navigate("/login");
+      setIsGuest(true);
     }
   }, []);
-
-  return (
-    <>
-      <div className="md:bg-white md:shadow-md sticky top-0 z-10">
-        <div className="container mx-auto">
-          <Navbar userData={userData} />
+  if (!token || isGuest) return <LoadingFull />;
+  else
+    return (
+      <>
+        <div className="md:bg-white md:shadow-md sticky top-0 z-10">
+          <div className="container mx-auto">
+            <Navbar userData={userData} />
+          </div>
         </div>
-      </div>
-      <Outlet />
-    </>
-  );
+        <Outlet />
+      </>
+    );
 }
 
 export default Public;
