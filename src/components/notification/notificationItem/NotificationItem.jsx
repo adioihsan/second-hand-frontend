43b48f3b000ce2b2982975722008b5d7bfcdfe17
adioihsan <adioihsan@gmail.com>
2022-07-22 00:07:40 +0000

    Add notification for accepted nego and sold product

--- src/components/notification/notificationItem/NotificationItem.jsx
+++ src/components/notification/notificationItem/NotificationItem.jsx
@@ -1,10 +1,18 @@
 import Dot from "../../dot/Dot";
 import "./notificationItem.css";
 function NotificationItem({ notif }) {
+  console.log(notif);
   return (
     <div className="notificationItem">
       <div>
-        <img src="/assets/images/product.png" />
+        <img
+          src={
+            process.env.REACT_APP_STORAGE_URL +
+            "/images/" +
+            notif.product.images_url.split(",")[0]
+          }
+          alt={notif.name}
+        />
       </div>
       <div className="w-full">
         <div className="flex justify-between text-sm text-gray-500">
@@ -21,7 +29,14 @@ function NotificationItem({ notif }) {
           </div>
         </div>
         <p className="font-medium">{notif.product.name}</p>
-        <p className="font-medium">
+        <p
+          className={
+            (notif.nego_id && notif.status === "accepted") ||
+            notif.status === "done"
+              ? "font-medium line-through"
+              : "font-medium"
+          }
+        >
           {" "}
           {notif.product.price.toLocaleString("id-ID", {
             style: "currency",
@@ -37,6 +52,36 @@ function NotificationItem({ notif }) {
             })}{" "}
           </p>
         )} */}
+        {notif.nego_id && notif.status === "accepted" && (
+          <>
+            <p className="font-medium">
+              Berhasil Ditawar{" "}
+              {notif.price.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}{" "}
+            </p>
+            <p className="text-xs text-gray-600 ">
+              Kamu akan segera di hubungi penjual via whatsapp
+            </p>
+          </>
+        )}
+        {notif.nego_id && notif.status === "done" && (
+          <>
+            <p className="font-medium">
+              Berhasil Ditawar{" "}
+              {notif.price.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}{" "}
+            </p>
+            <p className="text-xs text-gray-600 ">
+              Yeay transaksi kamu berhasil, penjual sudah mengubah status
+              transaksi ke selesai
+            </p>
+          </>
+        )}
+
         {notif.nego_price && (
           <p className="font-medium">
             Ditawar{" "}
